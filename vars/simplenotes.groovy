51be4d7ac51532df3c2792b7e6414c707952f4d6
William Hayward <fml3956303@FED.CCLRC.AC.UK>
2021-03-05 17:45:25 +0000

    testing

--- vars/simplenotes.groovy
+++ vars/simplenotes.groovy
@@ -12,22 +12,23 @@ def call() //Map config=[:]
     print(pwd())
 
     def dir = new File(pwd());
+    def myfile = new File(dir.path + '/releasenotes.txt')
 
     println("can you see this 2?")
 
-    // new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
-    // {
-    //     writer ->
-    //         // list of file names and sizes (for files, no sizes for directories).
-    //         dir.eachFileRecurse(FileType.ANY){ file ->
-    //             if (file.isDirectory())
-    //             {
-    //                 writer.writeLine(file.name);
-    //             }
-    //             else
-    //             {
-    //                 writer.writeLine('\t' + file.name + '\t' + file.length());
-    //             }
-    //         }
-    // }
+    new File(myfile).withWriter('utf-8')
+    {
+        writer ->
+            // list of file names and sizes (for files, no sizes for directories).
+            dir.eachFileRecurse(FileType.ANY){ file ->
+                if (file.isDirectory())
+                {
+                    writer.writeLine(file.name);
+                }
+                else
+                {
+                    writer.writeLine('\t' + file.name + '\t' + file.length());
+                }
+            }
+    }
 }
