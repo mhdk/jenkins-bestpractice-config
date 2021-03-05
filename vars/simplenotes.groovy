ed4feef0d4697d1e1906a3a0e6ecc5c2e919b5d5
William Hayward <fml3956303@FED.CCLRC.AC.UK>
2021-03-05 15:56:13 +0000

    adding simpler version of releasenotes for testing

---
+++ vars/simplenotes.groovy
@@ -0,0 +1,28 @@
+#!/usr/bin/env groovy
+
+// Note we have changed from echo to writeline statements for writing to a file.
+
+import groovy.io.*;
+import java.io.*;
+
+@NonCPS
+def call(Map config=[:])
+{
+    def dir = new File(pwd());
+
+    new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
+    {
+        writer ->
+            // list of file names and sizes (for files, no sizes for directories).
+            dir.eachFileRecurse(FileType.ANY){ file ->
+                if (file.isDirectory())
+                {
+                    writer.writeLine(file.name);
+                }
+                else
+                {
+                    writer.writeLine('\t' + file.name + '\t' + file.length());
+                }
+            }
+    }
+}
